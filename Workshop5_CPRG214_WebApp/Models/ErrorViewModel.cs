4628224e29f8539c18fa9756796a89ca8ab2f290
Yogesh Patel <patelyogesh.mca@gmail.com>
2019-06-06 09:53:38 -0600

    Add project files.

---
+++ Workshop5_CPRG214_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Workshop5_CPRG214_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
